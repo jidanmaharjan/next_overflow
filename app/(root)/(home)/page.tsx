8887e1a62e061d90c4430a9bd4214316b9c86b8f
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-03-28 22:20:39 +0545

    Routing setup

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div  className="">
+        <h1 className="h1-bold">Let&apos;s go!!!</h1>
+      </div>
+    );
+  }
+  
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Let&apos;s go!!!</h1>
-    </div>
-  );
-}
